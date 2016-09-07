777d259c2c2a953aafa5ad1a025b279ec6c64ee0
MagB <maggie.bartkowska@utoronto.ca>
2016-09-07 15:11:51 -0400

    new scripts to summarize diversity and pariwise differences

---
+++ scripts/preliminary_diversity.R
@@ -0,0 +1,244 @@
+install.packages("ggplot2")
+library(ggplot2)
+library(gridExtra)
+library(grid)
+het_homs_calls=read.table("output/summary_stats/Het_Hom_alt_Missing_counts_by_sample.txt", header=T, sep="\t")
+
+summary(het_homs_calls)
+#range of het proportion
+summary(het_homs_calls$Prop_het)
+#10 pop groups ignoring year and elevation
+#organize samples into populations
+Kidd=het_homs_calls[grepl("Kd",het_homs_calls$Sample_ID),]
+NkN=het_homs_calls[grepl("NkN",het_homs_calls$Sample_ID),]
+NkS=het_homs_calls[grepl("NkS",het_homs_calls$Sample_ID),]
+APK=het_homs_calls[grepl("APK",het_homs_calls$Sample_ID),]
+APR=het_homs_calls[grepl("APR",het_homs_calls$Sample_ID),]
+HB=het_homs_calls[grepl("HB",het_homs_calls$Sample_ID),]
+Kob=het_homs_calls[grepl("Kob",het_homs_calls$Sample_ID),]
+Pho=het_homs_calls[grepl("Pho",het_homs_calls$Sample_ID),]
+SP=het_homs_calls[grepl("SP",het_homs_calls$Sample_ID),]
+SS=het_homs_calls[grepl("SS",het_homs_calls$Sample_ID),]
+
+
+Kidd_plot=ggplot(Kidd, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Kidd")
+
+NkN_plot=ggplot(NkN, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("NkN")
+
+NkS_plot=ggplot(NkS, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("NkS")
+
+APK_plot=ggplot(APK, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("APK")
+
+APR_plot=ggplot(APR, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("APR")
+                
+HB_plot=ggplot(HB, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+    
+          panel.background=element_blank(),  
+                axis.line = element_line(colour = "black"))+
+                theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+                ylab("Proportion heterozygote calls") +
+                ggtitle("HB")
+
+Kob_plot=ggplot(Kob, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Kob")
+                
+Pho_plot=ggplot(Pho, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+        axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Pho")
+
+SP_plot=ggplot(SP, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x.x = element_text(size=14,angle = 270, hjust=1),
+
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("SP")
+
+SS_plot=ggplot(SS, aes(x = Sample_ID, y = Prop_het)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("SS")
+
+grid.arrange(Kidd_plot,NkN_plot, ncol=1)
+grid.arrange(NkS_plot, APK_plot, ncol=1)
+grid.arrange( APR_plot, HB_plot, Kob_plot,Pho_plot, ncol=2)
+grid.arrange(SP_plot, SS_plot, ncol=2)
+
+
+#Proportion Alternate genotypes:
+
+Kidd_plot=ggplot(Kidd, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Kidd")
+
+NkN_plot=ggplot(NkN, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("NkN")
+NkS_plot=ggplot(NkS, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("NkS")
+
+APK_plot=ggplot(APK, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("APK")
+
+APR_plot=ggplot(APR, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("APR")
+
+HB_plot=ggplot(HB, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("HB")
+
+Kob_plot=ggplot(Kob, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Kob")
+
+Pho_plot=ggplot(Pho, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("Pho")
+
+SP_plot=ggplot(SP, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x.x = element_text(size=14,angle = 270, hjust=1),
+        
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("SP")
+
+SS_plot=ggplot(SS, aes(x = Sample_ID, y = Prop_alt)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("Proportion heterozygote calls") +
+        ggtitle("SS")
+
+grid.arrange(Kidd_plot,NkN_plot, ncol=1)
+grid.arrange(NkS_plot, APK_plot, ncol=1)
+grid.arrange( APR_plot, HB_plot, Kob_plot,Pho_plot, ncol=2)
+grid.arrange(SP_plot, SS_plot, ncol=2)
+
+#distribution of SNP sites in each samples
+pop_codes=c("Kd", "NkN", "NkS", "APK", "APR", "HB", "Kob", "Ph", "SP", "SS" )
+sites=c("9359", "9359", "6317", "2227", "3050", "6074", "2362", "2071", "5547", "8844")
+pop_sites=data.frame(pop_codes, sites)
+pop_sites$sites=as.numeric(as.character(pop_sites$sites))
+mean(pop_sites$sites)
+
+ggplot(pop_sites, aes(x = pop_codes, y = sites)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(size=14,angle = 270, hjust=1),
+        panel.background=element_blank(),  
+        axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("# SNP sites") + xlab("Population")+
+     #   ylim(0, 8000)+
+        geom_hline(yintercept=5521)
+
+pairs <- read.table(pipe('pbpaste'),header=F, sep="")
+colnames(pairs)
+colnames(pairs)=c("pop1", "pop2", "shared_sites", "pop1_count", "pop2_count", "fraction_pop1", "fraction_pop2")
+
+pairs[grepl("Kob",pairs$pop1) | grepl("Kob",pairs$pop2) ,]
+
+#association between no samples at site and sample number
+sites_no_data=read.table(pipe('pbpaste') , header=F, sep="")
+ggplot(sites_no_data, aes(x=V3, y=V2)) + geom_point()+
+        theme( text = element_text(size=14),
+                panel.background = element_blank(),
+                axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        ylab("# sites with no data") + xlab("Sample size within pop")
+
---
+++ scripts/sample_depth.R
@@ -0,0 +1,78 @@
+library(ggplot2)
+
+#summarize read length count after demultiplexing:
+read_lengths=read.table("output/read_lenght_counts_post_ustack/read_length_count_by_sample2.txt",header=F, sep=" ")
+colnames(read_lengths)=c("read_count", "read_length", "sample_ID")
+
+
+
+ggplot(read_lengths, aes(x = sample_ID, y = read_count)) + geom_bar(stat = "identity") +
+        theme(axis.text = element_text(size=14),text=element_text(size=15),
+                axis.text=element_text(size=14), 
+                panel.background=element_blank(),  
+                axis.line = element_line(colour = "black"))+
+        theme(axis.line.x = element_line(color="black"),
+                axis.line.y = element_line(color="black"))+
+        xlab("Sample") + ylab("Read count") +
+        scale_y_continuous(expand = c(0,0)) +
+        ggtitle("Average read number per sample following demultiplexing" )
+
+new_name=paste(i,".pdf", sep="")
+
+
+#Average number of reads per individual
+mean(read_lengths$read_count, na.rm =T)
+summary(read_lengths$read_count,digits=max(10, getOption("digits")))
+
+#samples in the lowest quantile
+read_lengths[read_lengths$read_count<2523975,c(1,3)]
+
+#samples is highest quantile
+read_lengths[read_lengths$read_count>4035963,c(1,3)]
+
+file_list <- list.files(path="depth", pattern="*.txt", full.names=T, recursive=FALSE)
+i="depth/2015SPLo16.matches.tsv_depth.txt"
+list_samp_mean={}
+for( i in file_list){
+        
+       temp_file= read.table(i, header = T, sep=" ")
+       #find mean depth
+       if(i !="all_samples_depth.txt"){
+               
+       
+       samp_mean=round(sum((temp_file$depth*temp_file$count))/sum(temp_file$count), digits=2)
+       sample_name=unlist(strsplit(i,"matches.tsv"))
+       sample_name2=unlist(strsplit(sample_name[1], "/"))
+       title_name=paste(sample_name2[2], "mean=",samp_mean,  sep=" " )
+       
+       }  else{title_name=paste(i,samp_mean, sep=" " )}
+       temp_plot=ggplot(temp_file, aes(x = depth, y = count)) + geom_bar(stat = "identity") +
+                       theme(axis.text = element_text(size=14),text=element_text(size=15),
+                               axis.text=element_text(size=14), 
+                               panel.background=element_blank(),  
+                               axis.line = element_line(colour = "black"))+
+                       theme(axis.line.x = element_line(color="black"),
+                               axis.line.y = element_line(color="black"))+
+                        xlab("Depth") + ylab("Count") +
+                       
+                       scale_y_continuous(expand = c(0,0)) +
+                       scale_x_continuous(limits=c(0,100), breaks=seq(0,100,by=10)) +
+                        ggtitle(title_name)
+       
+               new_name=paste(i,".pdf", sep="")
+       ggsave(filename=new_name, plot=temp_plot,  device="pdf")
+
+       list_samp_mean=rbind(list_samp_mean,c(sample_name2[2], samp_mean))
+}
+list_samp_mean=as.data.frame(list_samp_mean)
+colnames(list_samp_mean)=c("Sample_ID", "Depth")
+summary(list_samp_mean$Depth)
+list_samp_mean$Depth=as.numeric(as.character(list_samp_mean$Depth))
+
+#Average depth after applying 80% genotype at site filter:
+samp_dep_filter=read.table("output/depth_80_percent_genotypes_at_site.txt", header=T, sep=" ")
+
+ggplot(samp_dep_filter, aes(x = Sample_ID, y = Avg_depth)) + geom_bar(stat = "identity") 
+
+summary(samp_dep_filter$Avg_depth)
+
