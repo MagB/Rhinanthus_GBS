c2398ac6b8ea0ae04a4fe9352729460ecde91198
MagB <maggie.bartkowska@utoronto.ca>
2016-11-11 15:30:51 -0500

    added NJ tree and pairwise comparison script to folder

---
+++ scripts/NJ_tree_PCA.R
@@ -0,0 +1,173 @@
+#In this script, I generate 2 PCA plots and a NJ tree based on euclidean distance.
+#To generate the PCA and the NJ tree I convert my snp data from a .summary file format to a .snp file format
+#the adegenet package and functions like pca require that snp data be converted into a genligth or gen object.
+
+#this is the path to an example of the .snp file format
+#file.show(system.file("files/exampleSnpDat.snp",package="adegenet"))
+# See this tutorial for details on this package
+#https://github.com/thibautjombart/adegenet/wiki/Tutorials
+#I have a copy here file:///Users/Maggie/Dropbox/Spirodela%20diversity/PCA/tutorial-genomics.pdf
+install.packages("adegenet")
+install.packages("ape")
+library("adegenet")
+citation(package = "adegenet", lib.loc = NULL)
+citation(package = "base", lib.loc = NULL)
+citation("ape")
+library(help = adegenet )
+library(ape)
+R.Version() 
+search()
+?"adegenet"
+mygenos=read.snp("output/snp_files/batch1.snp")
+
+#alleles(mygenos36_scaff1)=rep("a/t", 417867)
+
+#You don't need populations for the glpca or the k-means. populations are required for doing a dpca and some other functions
+#If pops are required be careful of how they are defined.
+pop(mygenos)
+
+
+mygenos$pop
+names(mygenos)
+
+#this was a plot of snp density, which can be done if you have unique sequential positions for all snps.
+#max(mygenos36_scaff1$position)
+#snpposi.plot(position(mygenos36_scaff1), genome.size=8941088)
+#glPlot(mygenos36_scaff1, posi="topleft")
+
+pca1 <- glPca(mygenos)
+8#I chose to retain 4 axes. 
+pca1$eig
+pca1$scores
+pca1$loadings
+pca1$sdev
+scatter(pca1)
+?ade4
+#get %variance explained:
+variance_explained=pca1$eig/sum(pca1$eig)
+print("Var explained")
+print(c("PCA1, PCA2, PCA3"))
+
+print(c(variance_explained[1], variance_explained[2],variance_explained[3]))
+
+#Here I make a composite plot of a NJ tree and a PCA plot such that the colours and labels on the tree match that of the PCA
+library(ape)
+# The defualt uses the Euclidean distance on allele frequencies. Originally I used raw genetic distance calculated as the average of the per locus distance 
+#for pairs of individuals. This explains why the new nj tree looks nicer.
+tre <- nj(dist(as.matrix(mygenos)))
+plot(tre, typ="phylogram",cex=0.41,label.offset=8)
+
+
+
+#colors are obtained from using the colorplot function. 
+myCol2 <- colorplot(pca1$scores[,1:2],pca1$scores[,1:2],transp=TRUE, cex=1, ylab="PCA3", xlab="PCA2")
+
+#the list of colors is just the vector myCol2, but here I can more easily alter colours for specific samples
+#myCols_unrotated_tree_colors=c("#E94E0080" ,"#2F060080", "#360D0080", "#2B000080", "#C58C0080", "#D9CE0080", "#C28B0080", "#D5C90080","#BAB10080", "#DACF0080", "#DDD20080", "#FF810080", "#FA800080", "#B1592080", "#B1592080", "#ED4B0080","#E1510080", "#EB4B0080", "#31080080", "#3A140080", "#51320080", "#C4950080", "#26C30080", "#19CD0080","#8D810080", "#F4470080", "#03DA0080", "#00DB0080", "#5D240080", "#04D80080", "#58810080", "#581A0080", "#1FC80080", "#00DA0080", "#5D800080", "#5D7E0080")
+#Here I find the index of the sample I am interested in. Once I know the index I can pull out the color for that sample
+#my_list=which(tre$tip.label %in% tre$tip.label[grepl("RR3_6", tre$tip.label)])
+#myCols_unrotated_tree_colors[c(29,32)]="#000000"
+#myCols_unrotated_tree_colors[c(my_list)]
+
+
+
+
+#maybe label by area: 10 areas
+#first generate a list of regions to add onto or replace the tip labels
+my_region_list=c()
+for(i in tre$tip.label){
+       l= ifelse(grepl("Kd",i)==TRUE,"Kd", 
+                ifelse(grepl("Kob",i)==TRUE,"Kob",
+                        ifelse(grepl("SS",i)==TRUE, "SS",
+                                ifelse(grepl("SP",i)==TRUE, "SP",
+                                        ifelse(grepl("APR",i)==TRUE, "APR",
+                                                ifelse(grepl("NkS",i)==TRUE, "NkS",
+                                                        ifelse(grepl("NkN",i)==TRUE, "NkN",
+                                                                ifelse(grepl("HB",i)==TRUE, "HB",
+                                                                        ifelse(grepl("Ph",i)==TRUE, "Ph",
+                                                                                ifelse(grepl("APK",i)==TRUE, "APK",0))))))))))
+        my_region_list=c(my_region_list, l)
+}
+tre$tip.label=paste(c(tre$tip.label),my_region_list, sep=" ")
+tre$tip.label=my_region_list
+
+#HERE I START THE PLOTTING OF NJ AND PCA
+
+dev.off()
+quartz("new_fig", width=10, height=10)
+nf <- layout(matrix(c(1,1,2,3), 2, 2, byrow=FALSE), respect=TRUE)
+layout.show(nf)
+par(oma=c(1,2,2,1))
+par(mar=c(2,2,2,2))
+
+#first plot the NJ tree
+#I had used this object first, plot(tre, typ="phylogram",cex=0.81,label.offset=14)
+#Tree before rotation is tre
+plot(tre, typ="phylogram",cex=0.81,label.offset=14)
+
+
+#note: before running tiplabels run this line that appears below
+#to generate colors for each sample : myCol2 <- colorplot(pca1$scores[,2:3],pca1$scores[,2:3],transp=TRUE, cex=1, ylab="PCA3", xlab="PCA2")
+tiplabels(pch=15, col=myCol2, cex=1)
+mtext(side=3, line=-1, text="a)", adj=0, outer=T) 
+
+
+#next plot PCA 1 vs 2 and 2 vs 3
+plot(pca1$scores[,1],pca1$scores[,2], col=myCol2, pch=19, cex=0.7,)
+#myCol <- colorplot(pca1$scores[,1:2],pca1$scores[,1:2], transp=TRUE, cex=1)
+abline(h=0,v=0, col="grey")
+title(ylab="PCA2", xlab="PCA1")
+mtext(side=1, text='PCA1', cex=0.81, line=2)
+mtext(side=2, text='PCA2', cex=0.81, line=2)
+mtext(side=3, text="b)", line=1, adj=-0.4) 
+
+
+#Note you need to run the line the following line defining myCol2 to obtain colours for the group points
+#but plot PCA 3 vs PCA2 using the plot function
+#myCol2 <- colorplot(pca1$scores[,2:3],pca1$scores[,2:3],transp=TRUE, cex=1, ylab="PCA3", xlab="PCA2")
+
+
+plot(pca1$scores[,3],pca1$scores[,2], col=myCol2, pch=19,transp=TRUE, cex=0.7,)
+
+#text(pca1$scores[,3]~pca1$scores[,2], labels=rownames(pca1$scores), cex= 0.3, pos=pos_vector)
+abline(h=0,v=0, col="grey")
+mtext(side=1, text='PCA3', cex=0.81, line=2)
+mtext(side=2, text='PCA2', cex=0.81, line=2)
+mtext(side=3, text="c)", line=1, adj=-0.4) 
+
+quartz.save("output/PCA_tree/PCA_tree_default_stacks2.pdf", type = "pdf", device = dev.cur(), dpi = 600)
+
+dev.off()
+
+
+
+
+
+###KMeans clustering: This is an interactive function.
+#of assigning an individual sample to its genotype grouping. The maximum number of groups here is 18
+
+#I'm trying to find the % of variance explained to justify my choice of 20 PC.
+grp=find.clusters(mygenos36, clust=NULL, n.pca=NULL,n.clust=NULL, stat=c("BIC"),choose.n.clust=TRUE, criterion=c("diffNgroup"),max.n.clust=round(nInd(x)/2), n.iter=1e5,n.start=10,  scale=FALSE, pca.select=c("percVar"), perc.pca=NULL,glPca=NULL)
+#This function called find.clusters
+#here I keep 36 PC because there are not so many. I also keep 13 clusters because this is the lowest BIC value
+
+
+
+
+
+
+# Here I play with a function to look at group assigment. It's not hugely informative so I dropped this.
+#find.clusters() function.
+#NOTE WHEN I set the pop to be 13 groups identified using the cluster, then the probabiltiy of membership is 100%
+#so I dont think this is very informative. If I wanted probability of assigninment to region, then something informative happend
+#I have left the pop groupings as regions. As is currently the case in the original data.
+#in this case the probabiltiy of assignment was interesting but not hugely informative for the task at hand.
+#Just for kicks I redid this with the region assignment while biking.
+#mygenos36_for_group_assignment=mygenos36
+
+#pop(mygenos36_for_group_assignment)=as.vector(unlist(grp[3]))
+#pop(mygenos36_for_group_assignment)=mygenos36$pop
+
+
+
+
---
+++ scripts/pairwise_differences.R
@@ -0,0 +1,205 @@
+install.packages("xlsx")
+install.packages("dplyr")
+library(xlsx)
+library(dplyr)
+library(ggplot2)
+pairwise_differences=read.xlsx("output/summary_stats/pairwise_differences.xlsx", header=T, 1)
+
+head(pairwise_differences)
+str(pairwise_differences)
+pairwise_differences$samp1=as.character(pairwise_differences$samp1)
+pairwise_differences$samp2=as.character(pairwise_differences$samp2)
+
+pairwise_differences=mutate(pairwise_differences, N_same=N_both_het+N_hom_hom_ref+N_hom_hom_alt, N_different=N_hom_het+N_hom_alt_hom_ref)
+
+#avg distance to members of same pop , to other pops , to all samples
+
+#each sample average to all other samples
+i="2015KobHi14"
+avg_distances_data_all={}
+for(i in unique(c(pairwise_differences$samp1, pairwise_differences$samp2))){
+        temp=pairwise_differences[pairwise_differences$samp1==i | pairwise_differences$samp2==i,  ]
+        #I could also try doing a weighted contribution to differences?
+        avg_distance_het_hom=mean(temp$N_hom_het)
+        avg_distance_hom_alt=mean(temp$N_hom_alt_hom_ref)
+        avg_distance_total=mean(temp$N_different)
+        avg_num_where_both_have_data=mean(temp$num_sites_where_both_havedata)
+        
+        t=cbind(i, avg_distance_het_hom, avg_distance_hom_alt,avg_distance_total,avg_num_where_both_have_data)
+        avg_distances_data_all=rbind(avg_distances_data_all, t)
+}
+avg_distances_data_all=as.data.frame(avg_distances_data_all)
+colnames(avg_distances_data_all)=c("sample", "avg_distance_het_hom", "avg_distance_hom_alt","avg_distance_total","avg_num_where_both_have_data")
+str(avg_distances_data_all)
+avg_distances_data_all$avg_distance_het_hom=as.numeric(as.character(avg_distances_data_all$avg_distance_het_hom))
+avg_distances_data_all$avg_distance_hom_alt=as.numeric(as.character(avg_distances_data_all$avg_distance_hom_alt))
+
+avg_distances_data_all %>% summarise_each(funs(mean))
+class(avg_distances_data_all)
+#make new column of popnames
+
+
+pop_name_list=c("Kob", "Kd", "SS","APR","SP","NkN","APK","NkS","HB","Ph")
+
+avg_distances_data_within_pop={}
+distances_data_within_pop={}
+for(population_name in unique(c(pop_name_list))){
+        
+        temp=pairwise_differences[grep(population_name,pairwise_differences$samp1),]
+        temp2=temp[grep(population_name, temp$samp2),]
+        head(temp2)
+        distances_data_within_pop=rbind(distances_data_within_pop, temp2)
+        
+        #I could also try doing a weighted contribution to differences?
+        temp3=summarise_each(temp2, funs(mean))
+        
+        t=cbind(population_name, temp3[,3:12])
+        avg_distances_data_within_pop=rbind(avg_distances_data_within_pop, t)
+}
+
+#2014NkSML2605TGCGA
+#2014NkSML2605
+pairwise_differences[pairwise_differences$samp1=="2014NkSML2605" & pairwise_differences$samp2=="2014NkSML2605TGCGA",]
+
+pairwise_differences[pairwise_differences$samp1=="2014NkSML2605" & pairwise_differences$samp2=="2014NkSML2605TGCGA",]
+NkS_dups=pairwise_differences %>% filter((grepl("NkS",samp1) & grepl("NkS",samp2)))
+head(NkS_dups)
+ggplot(NkS_dups,aes(N_hom_het)) +geom_histogram()+ geom_vline(xintercept=26) +
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.1),
+                axis.line.y = element_line(color="black", size = 0.1))
+
+ggplot(NkS_dups,aes(N_hom_alt_hom_ref)) +geom_histogram()+ geom_vline(xintercept=26) +
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.1),
+                axis.line.y = element_line(color="black", size = 0.1))
+
+#there are 133 differences these are het to hom and hom to hom differences between samples that are supposed to be identical
+#but there are on average 78.16667 differences among samples within this population NKS, 47.73188 are hom to het and 30.43478 are hom to hom
+#but within the two "duplicated samples" there are 107 het to hom differences and 26 hom to hom differences. 
+#this suggests to me that either these two samples are not duplicates or we have high genotype calling errors. 
+# to figure out if its genotype calling errors we could pull out those sites that are het to hom and see if coverage is different at those sites relative to where
+#these two samples are homs 
+
+avg_distances_data_within_pop %>% select(population_name, N_hom_het, N_hom_alt_hom_ref)
+
+
+#avg distance betwwen pops
+avg_distances_data_between_pop={}
+distances_data_between_pop={}
+#population_name="Kd"
+population_name2="NkS"
+for(population_name in unique(c(pop_name_list))){
+        #pull out all comparisons between 2 pops
+        temp= pairwise_differences %>% filter(grepl(population_name,samp1) | grepl(population_name,samp2))
+        #remove within the same population comparison
+        temp2= temp %>% filter(!(grepl(population_name,samp1) & grepl(population_name,samp2)))
+        #now get the average distance of one pop to all others:
+        
+        
+        #I have to pull out now current focal pop and get average fot that to each other pop one ata time
+        for(population_name2 in unique(c(pop_name_list))){
+                if(population_name==population_name2) next
+                #pull out only data for population_name2
+                temp3= temp2 %>% filter(grepl(population_name2,samp1) | grepl(population_name2,samp2))
+                new_row1=cbind(population_name, population_name2,temp3)
+                distances_data_between_pop=rbind(distances_data_between_pop, new_row1)
+                
+                temp4=temp3 %>% summarise_each(funs(mean))
+                
+                        
+                temp5=temp3 %>% summarise(n=n())
+                new_row=cbind(population_name, population_name2,temp4[3:length(temp4)], temp5)
+                avg_distances_data_between_pop=rbind(avg_distances_data_between_pop, new_row)
+                
+        }
+}
+
+write.csv(avg_distances_data_between_pop, file = "output/avg_distances_data_between_pop.csv", row.names = FALSE)
+
+
+head(distances_data_between_pop)
+ggplot(distances_data_between_pop,aes(N_hom_het)) +geom_histogram()+ 
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.1),
+                axis.line.y = element_line(color="black", size = 0.1))
+
+
+ggplot(distances_data_between_pop,aes(N_hom_alt_hom_ref)) +geom_histogram()+ 
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.1),
+                axis.line.y = element_line(color="black", size = 0.1))
+
+#make overlapping distribution of het to hom differences
+between_pop=distances_data_between_pop %>%  mutate(type_dist = "between") %>% select(type_dist,N_hom_het)
+within_pop=distances_data_within_pop %>% mutate(type_dist = "within") %>% select(type_dist,N_hom_het)
+
+joined_data=bind_rows(between_pop,within_pop)
+
+ggplot(joined_data, aes(x=N_hom_het, fill=type_dist, color=type_dist)) + geom_histogram( position="identity", alpha=0.5)+
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.2),
+                axis.line.y = element_line(color="black", size = 0.2))+
+        scale_color_manual(values=c("#FF0000", "#0000FF"))+
+        scale_fill_manual(values=c("#FF0000", "#0000FF"))
+
+
+ggplot(joined_data, aes(x=N_hom_het, fill=type_dist, color=type_dist)) + geom_histogram(aes(y=..density..), position="identity", alpha=0.5)+
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.2),
+                axis.line.y = element_line(color="black", size = 0.2))+
+        scale_color_manual(values=c("#FF0000", "#0000FF"))+
+        scale_fill_manual(values=c("#FF0000", "#0000FF"))
+
+#make overlapping distribution of hom to hom differences
+between_pop2=distances_data_between_pop%>%  mutate(type_dist = "between")  %>% select(type_dist,N_hom_alt_hom_ref)
+within_pop2=distances_data_within_pop %>% mutate(type_dist = "within") %>% select(type_dist,N_hom_alt_hom_ref)
+
+joined_data2=bind_rows(between_pop2,within_pop2)
+
+ggplot(joined_data2, aes(x=N_hom_alt_hom_ref, fill=type_dist, color=type_dist)) + geom_histogram( position="identity", alpha=0.5)+
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.2),
+                axis.line.y = element_line(color="black", size = 0.2))+
+        scale_color_manual(values=c("#FF0000", "#0000FF"))+
+        scale_fill_manual(values=c("#FF0000", "#0000FF"))
+
+
+ggplot(joined_data2, aes(x=N_hom_alt_hom_ref, fill=type_dist, color=type_dist)) + geom_histogram(aes(y=..density..), position="identity", alpha=0.5)+
+        theme_bw() + 
+        theme(plot.background = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank() )+
+        theme(panel.border= element_blank())+
+        theme(axis.line.x = element_line(color="black", size = 0.2),
+                axis.line.y = element_line(color="black", size = 0.2))+
+        scale_color_manual(values=c("#FF0000", "#0000FF"))+
+        scale_fill_manual(values=c("#FF0000", "#0000FF"))
